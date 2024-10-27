4baa14005370eca28f00ca5b039c6578f6731a62
rodgers <rodgerstarrodgers@gmail.com>
2024-10-27 17:17:59 +0300

    portfolio container

--- main.js
+++ main.js
@@ -25,24 +25,51 @@ const ScrollRevealOption = {
 ScrollReveal().reveal(".header_image img",{
     ...ScrollRevealOption,
     origin: "right",
-})
-
+});
 ScrollReveal().reveal(".header_content .section_subheader ",{
     ...ScrollRevealOption,
     delay: 500,
-})
-
+});
 ScrollReveal().reveal(".header_content h1",{
     ...ScrollRevealOption,
     delay: 1000,
-})
-
+});
 ScrollReveal().reveal(".header_content .section_description ",{
     ...ScrollRevealOption,
     delay: 1500,
-})
-
+});
 ScrollReveal().reveal(".header_content .header_btn ",{
     ...ScrollRevealOption,
     delay: 2000,
+});
+ScrollReveal().reveal(".skill_content .section_header",{
+    ...ScrollRevealOption,
+});
+ScrollReveal().reveal(".skill_content .section_subheader",{
+    ...ScrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".skill_content .section_description",{
+    ...ScrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".skill_btn",{
+    ...ScrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".skill_card",{
+    duration: 1000,
+    interval: 500,
+    delay: 2000,
+});
+
+const portfolio = document.querySelector(".portfolio_image");
+
+const portfoliocontent = Array.from(".portfolio.children");
+
+portfoliocontent.forEach((item) => {
+    const duplicateNode = item.cloneNode(true);
+    duplicateNode.setAttribute("aria-hidden", true);
+    portfolio.appendChild(duplicateNode);
 })
